169abd62f4af7af239a23a75b188ecd02990729d
iamNajeeb <najeebgithub@gmail.com>
2024-02-28 00:05:05 +0500

    array method

---
+++ index.js
@@ -0,0 +1,8 @@
+var arr = "Apple";
+var arr = "Orange";
+var arr = "Grape";
+var arr = "Banana";
+console.log(arr);
+//customer perchansingn fruits just banana
+//use the pop method
+arr.pop(banana);
